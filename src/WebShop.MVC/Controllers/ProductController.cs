9e86ea9ba77d0a4899768c7d9dc3646dc678d26f
Studenti <student@tfzr.rs>
2025-11-05 14:27:12 +0100

    Dodat product kontroler i demo view

---
+++ src/WebShop.MVC/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebShop.MVC.Models.Product;
+
+namespace WebShop.MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<ProductViewModel> products =
+            [
+                new ProductViewModel
+                {
+                    Id = 1,
+                    Code = "001",
+                    Name = "RAM memorija"
+                },
+                new ProductViewModel
+                {
+                    Id = 2,
+                    Code = "002",
+                    Name = "LG monitor"
+                },
+            ];
+
+            return View(products);
+        }
+    }
+}
